51b48e420ee6229f56ef4ec391a70d263ccd2320
Daniel-LA0303 <03daniel030399@gmail.com>
2026-06-07 15:26:11 -0600

    feat: add commens

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,8 +5,8 @@ import mongoose from "mongoose";
 //2. conexion a la db
 const connectDB = async () => {
     try {
-            await mongoose.connect(process.env.MONGO_URL_WEB as string);
-        console.log('db conectada');
+        await mongoose.connect(process.env.MONGO_URL_WEB as string);
+        console.log('db conected');
     } catch (error: any) {
         console.log(`error: ${error.message}`);
         process.exit(1);
